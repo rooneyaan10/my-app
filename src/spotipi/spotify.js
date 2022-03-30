2906519580756de31556cb6ea0d2b9e788a170fb
Sharhan Anhar <jancoegsekali@gmail.com>
2022-03-30 16:11:01 +0700

    hide client id

--- src/spotipi/spotify.js
+++ src/spotipi/spotify.js
@@ -1,4 +1,4 @@
-var client_id = "decb34fa61bf4821807df455650ce396";
+var client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 var redirect_uri = "http://localhost:3000";
 
 var state = "KMG2FE1205SHARHAN";
